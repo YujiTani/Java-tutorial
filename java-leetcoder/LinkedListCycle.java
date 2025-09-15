e5a7e69b0378de3e2333e92e4055ec36aadba927
yuzunosk55 <yuzuha3ds@gmail.com>
2025-09-15 15:15:35 +0900

    learn java leetcode day1

--- java-leetcoder/LinkedListCycle.java
+++ java-leetcoder/LinkedListCycle.java
@@ -11,18 +11,23 @@ public class LinkedListCycle {
       int[] ints = parseStringToInteger(args);
       LinkedListCycle listCycle = new LinkedListCycle(ints);
       listCycle.list();
+      listCycle.reverseSort();
+      listCycle.list();
     } catch(NumberFormatException e) {
       System.err.println("無効な数値が含まれています");
     }
   }
 
   private List<Integer> data = new ArrayList<>();
+  private int index = 0;
 
   // コンストラクター
   public LinkedListCycle(int[] intArray) {
     for (int i : intArray) {
       data.add(i);
     }
+
+    this.index = intArray.length - 1; 
   }
 
   public void list() {
@@ -43,7 +48,6 @@ public class LinkedListCycle {
     return new int[0];
   }
 
-  // コンストラクターの受け取る値がStringかチェック
   private static boolean validation(String[] string) {
     if (string == null) {
       System.err.println("引数がnullになっています");
@@ -52,13 +56,46 @@ public class LinkedListCycle {
 
     if (!(string instanceof String[])) {
       System.err.println("引数が String[] ではありません。現在の型 "
-      + string.getClass().getName());
+        + string.getClass().getName());
+      return false;
+    }
+    return true;
+  }
+
+  private List<Integer> reverseSort() {
+    for(int i = this.data.size() - 1; i > 0; i--) {
+      for(int j = 0; j < this.data.size() - 1; j++){
+        if(this.data.get(j) < this.data.get(j + 1)) {
+          int tmp = this.data.get(j + 1);
+          this.data.set(j + 1, this.data.get(j));
+          this.data.set(j, tmp);
+        }
+      }
+    }
+    return this.data;
+  }
+
+  public boolean isNext() {
+    if(this.index == 0 && this.data.get(this.index) == this.data.get(0)) {
+      System.out.println("値は一つしかありません");
       return false;
     }
 
-
+    if (!(this.data.get(this.index - 1))) {
+      return false;
+    }
 
     return true;
+  }
+
+  public int next() {
+    if (this.index == 0) {
+      return this.data.get(this.data.size() - 1);
+    }
+
+    int res = this.data.get(index - 1);
+    this.index--;
+    return res;
   }
 }
 
