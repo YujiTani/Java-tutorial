22df7a51b3235d2cdbccd9a3c4c84b66511ae09a
yuzunosk55 <yuzuha3ds@gmail.com>
2025-09-19 09:04:08 +0900

    leetcode for java day5

--- java-leetcoder/1-two-sun/Solution.java
+++ java-leetcoder/1-two-sun/Solution.java
@@ -1,20 +1,18 @@
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.Map;
-
+import java.util.HashMap;
 
 public class Solution {
     int[] twoSum(int target, int[] nums) {
         Map<Integer, Integer> hashMap = new HashMap<>();
 
-        // 合わせて、13になるものが見つかればhashMapから index を取り出す。なければhashMapに追加。
-        for(int i = 0; i < nums.length -1; i++) {
+        for(int i = 0; i < nums.length -1; i++){
             int comprement = target - nums[i];
+
             if(hashMap.containsKey(comprement)) {
                 return new int[]{hashMap.get(comprement), i};
             } else {
                 hashMap.put(nums[i], i);
-                System.out.println(hashMap);
             }
         }
 
@@ -22,19 +20,18 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-        var solution = new Solution();
-        System.out.println(solution.getClass().getName());
+        Solution solution = new Solution();
+        int target = 9;
+        System.out.println("target" + target);
 
-        int target = 13;
-        int[] nums = {0,1,3,5,7,8,10,13,15};
+        int[] nums = {0,1,3,4,5,6,7,9};
         System.out.println("初期配列" + Arrays.toString(nums));
-
         int[] result = solution.twoSum(target, nums);
+
         if(result.length > 0) {
             System.out.println(Arrays.toString(result));
         } else {
-            System.err.print("エラー");
+            System.out.println("error");
         }
     }
-    
 }
